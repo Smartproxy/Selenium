6bfefa766711912e9c355e8e7108cd7f443e6269
MindaugasSP <123371655+MindaugasSP@users.noreply.github.com>
2023-01-25 17:51:55 +0200

    Update example.py
    Updated deprecated executable_path > Service object

--- python/example.py
+++ python/example.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium_python import get_driver_settings, smartproxy
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
 def webdriver_example():
-    driver = get_driver_settings()
-    if driver['DRIVER'] == 'FIREFOX':
-        browser = webdriver.Firefox(executable_path=r'{driver_path}'.format(driver_path=driver['DRIVER_PATH']), proxy=smartproxy())
-    elif driver['DRIVER'] == 'CHROME':
-        browser = webdriver.Chrome(executable_path=r'{driver_path}'.format(driver_path=driver['DRIVER_PATH']), desired_capabilities=smartproxy())
-    browser.get('http://ip.smartproxy.com/')
-
-    body_text = browser.find_element_by_tag_name('body').text
-
-    print(body_text)
-
+  driver = get_driver_settings()
+  if driver['DRIVER'] == 'FIREFOX':
+    browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()), proxy=smartproxy())
+  elif driver['DRIVER'] == 'CHROME':
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), desired_capabilities=smartproxy())
+  browser.get('http://ip.smartproxy.com/')
+  print(browser.page_source)
+  browser.close()
 if __name__ == '__main__':
-    webdriver_example()
+  webdriver_example()
