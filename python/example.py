ea65abba36dd87a2c92eda5cae36e09282bf0bc5
MindaugasSP <123371655+MindaugasSP@users.noreply.github.com>
2023-06-26 14:17:08 +0300

    Update example.py
    Updated to work with the latest Selenium version

--- python/example.py
+++ python/example.py
@@ -1,34 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.common.proxy import Proxy, ProxyType
+import time
 
-HOSTNAME = ''
-PORT = ''
-DRIVER = ''
+HOSTNAME = 'us.smartproxy.com'
+PORT = '10000'
+DRIVER = 'CHROME'
 
 def smartproxy():
-  prox = Proxy()
-  prox.proxy_type = ProxyType.MANUAL
-  prox.http_proxy = '{hostname}:{port}'.format(hostname = HOSTNAME, port = PORT)
-  prox.ssl_proxy = '{hostname}:{port}'.format(hostname = HOSTNAME, port = PORT)
-  if DRIVER == 'FIREFOX':
-    capabilities = webdriver.DesiredCapabilities.FIREFOX
-  elif DRIVER == 'CHROME':
-    capabilities = webdriver.DesiredCapabilities.CHROME
-  prox.add_to_capabilities(capabilities)
-  return capabilities
+    if DRIVER == 'FIREFOX':
+        options = FirefoxOptions()
+    elif DRIVER == 'CHROME':
+        options = ChromeOptions()
+    else:
+        raise ValueError("Invalid driver specified")
+
+    proxy_str = '{hostname}:{port}'.format(hostname=HOSTNAME, port=PORT)
+    options.add_argument('--proxy-server={}'.format(proxy_str))
+
+    return options
 
 def webdriver_example():
-  if DRIVER == 'FIREFOX':
-    browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()), proxy=smartproxy())
-  elif DRIVER == 'CHROME':
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), desired_capabilities=smartproxy())
-  browser.get('http://ip.smartproxy.com/')
-  print(browser.page_source)
-  browser.quit()
+    if DRIVER == 'FIREFOX':
+        browser = webdriver.Firefox(service=Service(GeckoDriverManager().install()), 
+                                    options=smartproxy())
+    elif DRIVER == 'CHROME':
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), 
+                                   options=smartproxy())
+    browser.get('http://ip.smartproxy.com/')
+    print(browser.page_source)
+    browser.quit()
 
 if __name__ == '__main__':
-  webdriver_example()
+    webdriver_example()
