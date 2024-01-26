e77432971a6105b1a799f16f05443a7f7db85d11
MindaugasSP <123371655+MindaugasSP@users.noreply.github.com>
2024-01-26 02:07:30 +0200

    Update example.js

--- nodejs/firefox/example.js
+++ nodejs/firefox/example.js
@@ -3,14 +3,14 @@ require('geckodriver');
 var webdriver = require('selenium-webdriver'), By = webdriver.By,
     until = webdriver.until,  firefox = require("selenium-webdriver/firefox"), proxy = require('selenium-webdriver/proxy');
 
-let addr = 'gate.smartproxy.com:7000'
+let addr = 'gate.smartproxy.com:7000'    //Proxy host:port configuration
 
 var driver = new webdriver.Builder()
  .forBrowser('firefox')
  .setProxy(proxy.manual({http: addr}))
  .build();
 
-driver.get('http://ip.smartproxy.com/')
+driver.get('http://ip.smartproxy.com/')  //Target website
 
 var textPromise = driver.findElement(webdriver.By.css('body')).getText();
 textPromise.then((text) => {
