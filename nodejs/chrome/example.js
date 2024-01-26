4c8f364799c5034bbdda375a1d593eaa05bce45c
MindaugasSP <123371655+MindaugasSP@users.noreply.github.com>
2024-01-26 14:36:47 +0200

    Update example.js

--- nodejs/chrome/example.js
+++ nodejs/chrome/example.js
@@ -1,19 +1,19 @@
 require('chromedriver');
 
 var webdriver = require('selenium-webdriver'), By = webdriver.By,
-    until = webdriver.until,  chrome = require("selenium-webdriver/chrome");
+    until = webdriver.until,  chrome = require("selenium-webdriver/chrome");  //Configures the Chromedriver
 
-let addr = 'gate.smartproxy.com:7000'          //Proxy host:port configuration
+let addr = 'gate.smartproxy.com:7000'                                         //Proxy host:port configuration
 let opt = new chrome.Options().addArguments(`--proxy-server=http://${addr}`)
 
-var driver = new webdriver.Builder()
+var driver = new webdriver.Builder()                                          //Initializes the configured Chromedriver
  .forBrowser('chrome')
  .setChromeOptions(opt)
  .build();
 
-driver.get('http://ip.smartproxy.com/')        //Target website
+driver.get('http://ip.smartproxy.com/')                                       //Target website
 
-var textPromise = driver.findElement(webdriver.By.css('body')).getText();
+var textPromise = driver.findElement(webdriver.By.css('body')).getText();     //Select desired Element from your target website
 textPromise.then((text) => {
   console.log(text);
 });
