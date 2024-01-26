121039cd21262d69fc6fcbe4e4977344b29bcc18
MindaugasSP <123371655+MindaugasSP@users.noreply.github.com>
2024-01-26 14:39:04 +0200

    Update example.js

--- nodejs/firefox/example.js
+++ nodejs/firefox/example.js
@@ -1,18 +1,18 @@
 require('geckodriver');
 
 var webdriver = require('selenium-webdriver'), By = webdriver.By,
-    until = webdriver.until,  firefox = require("selenium-webdriver/firefox"), proxy = require('selenium-webdriver/proxy');
+    until = webdriver.until,  firefox = require("selenium-webdriver/firefox"), proxy = require('selenium-webdriver/proxy'); //Configures the Geckodriver
 
-let addr = 'gate.smartproxy.com:7000'    //Proxy host:port configuration
+let addr = 'gate.smartproxy.com:7000'                                                                                       //Proxy host:port configuration
 
-var driver = new webdriver.Builder()
+var driver = new webdriver.Builder()                                                                                        //Initializes the configured Geckodriver
  .forBrowser('firefox')
  .setProxy(proxy.manual({http: addr}))
  .build();
 
-driver.get('http://ip.smartproxy.com/')  //Target website
+driver.get('http://ip.smartproxy.com/')                                                                                     //Target website
 
-var textPromise = driver.findElement(webdriver.By.css('body')).getText();
+var textPromise = driver.findElement(webdriver.By.css('body')).getText();                                                   //Select desired Element from your target website
 textPromise.then((text) => {
   console.log(text);
 });
